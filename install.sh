b04cf2a36b60112ca9eb256ad5792a93fdc117af
therealonenick <62951829+therealonenick@users.noreply.github.com>
2020-03-31 16:13:55 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VIMPERF = "$HOME/.vimrc"
+if test -f "$VIMPERF"; then
+	echo "$VIMPERF Exists.. Deleting..."
+    rm -rf  $VIMPERF
+	echo "Deleted."
+else
+	echo "No vimrc exists... Creating..."
+	cp ./.vimrc $HOME/.vimrc
+	#touch $VIMPERF
+	#echo "set number" >> $VIMPERF
+	#echo "set tabstop=4" >> $VIMPERF
+	#echo "set cursorline" >> $VIMPERF
+	echo "Done."
+fi
