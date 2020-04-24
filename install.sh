a7e141a14b53c7c953c1c7097b7178e91512f4ce
root <root@cloudlab.ucdevx.com>
2020-04-24 09:05:44 -0400

    Updated syntax highlighting for Bash and vim to remove harsh blue

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 VIMPERF="$HOME/.vimrc"
 BASHRC="$HOME/.bashrc"
+
+# Check if existing file and move it to retain.
 if test -f "$VIMPERF"; then
 	echo "$VIMPERF Exists.. moving..."
     mv $VIMPERF $HOME/.vimrc_ORIGINAL
@@ -13,8 +15,14 @@ fi
 cp ./.vimrc $HOME/.vimrc
 echo "Done."
 
+# Add vim alias for via
 echo "Adding alias to $BASHRC ..."
 echo "alias vi=vim" >> $BASHRC
+
+#Uncomment to add colors. This changes from the default blue which is sometimes hard to read
+echo "LS_COLORS=\$LS_COLORS:'di=0;94:';  export LS_COLORS" >> $BASHRC
+
+#
 
 echo "Done."
 echo ""
